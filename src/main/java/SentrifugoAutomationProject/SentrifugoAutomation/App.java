a7dd5a0c6362e46c884d34089764b9a064795683
adityamansana <adityamansana@yahoo.com>
2019-06-25 08:50:51 +0530

    Creating a master.

---
+++ src/main/java/SentrifugoAutomationProject/SentrifugoAutomation/App.java
@@ -0,0 +1,13 @@
+package SentrifugoAutomationProject.SentrifugoAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
